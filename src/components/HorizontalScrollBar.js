e5f8f93a3f2aa66197d0eb88e793e00e1f207075
Etienne <gwiavanderetienne2@gmail.com>
2023-04-28 15:11:29 +0100

    Horizontal scrolling

--- src/components/HorizontalScrollBar.js
+++ src/components/HorizontalScrollBar.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Box } from '@mui/material'
 import BodyParts from './BodyParts'
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import "react-horizontal-scrolling-menu/dist/styles.css";
 
-const HorizontalScrollBar = ({data }) => {
+// to initiate the horizontal scrolling we introduce the above imports from the 'react-horizontal-scrolling-menu'
+// was problematic at first till I used import "react-horizontal-scrolling-menu/dist/styles.css" from https://codesandbox.io/s/react-horizontal-scrolling-menu-v2-basic-example-swg0y?file=/src/index.tsx:61-199
+const HorizontalScrollBar = ({data,  bodyPart, setBodyPart}) => {
   return (
-    <Box>
+    <ScrollMenu >
         {data.map((item) =>(
             <Box 
                 key={item.id || item}
@@ -12,10 +16,10 @@ const HorizontalScrollBar = ({data }) => {
                 title={item.id || item}
                 m='0 40px'
             >
-                <BodyParts item={item}/>
+                <BodyParts item={item} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
             </Box>
         ))}
-    </Box>
+    </ScrollMenu>
   )
 }
 
